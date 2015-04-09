52378611cac6bcd6f4672d26bc86e80ab11fb7de
Brandon Sneed <brandon@redf.net>
2015-04-09 11:20:32 -0700

    Initial Commit

---
+++ THGJSON/THGJSON.h
@@ -0,0 +1,19 @@
+//
+//  THGJSON.h
+//  THGJSON
+//
+//  Created by Brandon Sneed on 4/9/15.
+//  Copyright (c) 2015 TheHolyGrail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for THGJSON.
+FOUNDATION_EXPORT double THGJSONVersionNumber;
+
+//! Project version string for THGJSON.
+FOUNDATION_EXPORT const unsigned char THGJSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <THGJSON/PublicHeader.h>
+
+
